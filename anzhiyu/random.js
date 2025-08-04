1d44c4881aedf11c3fcd16099472b0187a4dba6f
Elegy17 <2517518603@qq.com>
2025-08-04 23:13:25 +0800

    Site updated: 2025-08-04 23:13:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/04/hello-world/"];function toRandomPost(){
+var posts=["2025/08/04/第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
